0a8e1c94f0ac1f9f75bfbc3ff7b6885880add3c5
JenniferVeloD <jennifervelo2004@gmail.com>
2026-03-23 20:01:33 -0600

    Endpoint top 5 libros

--- routers/reportes.py
+++ routers/reportes.py
@@ -7,6 +7,7 @@ Requerimientos:
   R9.4 Estadísticas generales del sistema
 """
 from fastapi import APIRouter, Depends
+from sqlalchemy import func, desc
 from sqlalchemy.orm import Session
 from database import get_db, Prestamo, Libro, Usuario
 
@@ -16,7 +17,32 @@ router = APIRouter()
 
 @router.get("/top-libros", summary="R9.1 Top 5 libros más prestados")
 def top_libros(db: Session = Depends(get_db)):
-    pass
+    """Recuperar los préstamos agrupados por libro_id y contar las veces que se ha prestado cada libro"""
+
+    #Recuperar todos los préstamos y contar las veces que se ha prestado cada libro
+    prestamos = db.query(Prestamo).all()
+    veces_prestados = {} 
+
+    for prestamo in prestamos:
+        libro_id = prestamo.libro_id
+        if libro_id not in veces_prestados:
+            veces_prestados[libro_id] = 0  
+        veces_prestados[libro_id] += 1 
+
+    #Ordenar de mayor a menor y tomar los primeros 5
+    veces_prestados_ordenado = sorted(veces_prestados.items(), key=lambda x: x[1], reverse=True)
+    top_5 = veces_prestados_ordenado[:5]
+    resultado = []
+
+    #Recuperar los detalles de cada libro del top 5
+    for libro_id, total_prestamos in top_5:
+        libro = db.query(Libro).filter(Libro.id == libro_id).first()
+
+        if libro:
+            resultado.append({"libro_id": libro.id, "titulo": libro.titulo, "autor": libro.autor, 
+                              "isbn": libro.isbn,"cantidad": libro.cantidad, "prestamos": total_prestamos})
+
+    return {"top_libros": resultado}
 
 @router.get("/usuarios-activos", summary="R9.2 Usuarios con más préstamos")
 def usuarios_activos(db: Session = Depends(get_db)):
