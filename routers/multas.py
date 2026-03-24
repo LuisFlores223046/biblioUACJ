e9027946dd678074bec8b49db78e2d02c98f482f
Enrique Pereda <al223121@alumnos.uacj.mx>
2026-03-23 19:51:56 -0600

    Endpoint para registrar pagos de multa

--- routers/multas.py
+++ routers/multas.py
@@ -6,6 +6,8 @@ Requerimientos:
   R7.3 Registrar pago de multa
   R7.4 Ver total de multas del sistema
 """
+from http.client import HTTPException
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from datetime import datetime
@@ -28,7 +30,27 @@ def multas_usuario(usuario_id: int, db: Session = Depends(get_db)):
 
 @router.put("/pagar/{prestamo_id}", summary="R7.3 Registrar pago")
 def pagar_multa(prestamo_id: int, db: Session = Depends(get_db)):
-    pass
+    # Obtiene el préstamo por su ID
+    prestamo = db.query(Prestamo).filter(Prestamo.id == prestamo_id).first()
+    # Valida que el préstamo exista
+    if not prestamo:
+        raise HTTPException(status_code=404, detail="Préstamo no encontrado")
+    
+    #Valida si se tiene una multa pendiente
+    if prestamo.multa <= 0:
+        return {"message": "El préstamo no tiene multas pendientes", "multa_actual": prestamo.multa}
+    
+    #Guarda el monto de la multa antes de pagarla
+    monto_pagado = prestamo.multa
+    #Registra el pago y actualiza la multa a 0
+    prestamo.multa = 0.0
+
+    #Guarda los cambios en la base de datos
+    db.commit()
+    db.refresh(prestamo)
+
+    #Respuesta con el resultado del pago
+    return {"status": "success", "message": f"Multa de {monto_pagado} pagada correctamente","nuevo_saldo_multa": prestamo.multa }
 
 @router.get("/total", summary="R7.4 Total de multas")
 def total_multas(db: Session = Depends(get_db)):
