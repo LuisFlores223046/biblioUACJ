c98560d16d8e16ad0790d448c2f718598a9de534
JenniferVeloD <jennifervelo2004@gmail.com>
2026-03-23 20:39:27 -0600

    Implementacion del endpoint: Estadísticas generales del sistema

--- routers/reportes.py
+++ routers/reportes.py
@@ -18,7 +18,7 @@ router = APIRouter()
 @router.get("/top-libros", summary="R9.1 Top 5 libros más prestados")
 def top_libros(db: Session = Depends(get_db)):
     """Recuperar los préstamos agrupados por libro_id y contar las veces que se ha prestado cada libro"""
-
+    
     #Recuperar todos los préstamos y contar las veces que se ha prestado cada libro
     prestamos = db.query(Prestamo).all()
     veces_prestados = {} 
@@ -54,4 +54,31 @@ def prestamos_por_fecha(fecha: str, db: Session = Depends(get_db)):
 
 @router.get("/estadisticas", summary="R9.4 Estadísticas generales")
 def estadisticas(db: Session = Depends(get_db)):
-    pass
+    """Recuperar el total de libros, usuarios y préstamos, y separar préstamos activos y devueltos para 
+    generar un resumen general del sistema"""
+
+    #Recuperar el total de libros, usuarios y préstamos
+    libros = db.query(Libro).all()
+    total_libros = len(libros)
+    usuarios = db.query(Usuario).all()
+    total_usuarios = len(usuarios)
+    prestamos = db.query(Prestamo).all()
+    total_prestamos = len(prestamos)
+    prestamos_activos = 0
+    prestamos_devueltos = 0
+
+    #Separar préstamos activos y devueltos
+    for prestamo in prestamos:
+        if prestamo.fecha_devolucion is None:  
+            prestamos_activos += 1
+        else:
+            prestamos_devueltos += 1
+
+    #Devolver resumen general del sistema
+    return {
+        "total_libros": total_libros,
+        "total_usuarios": total_usuarios,
+        "total_prestamos": total_prestamos,
+        "prestamos_activos": prestamos_activos,
+        "prestamos_devueltos": prestamos_devueltos
+    }
